656f3bfd17f1cb24285f3d81cfe4c5b04e34b023
sehyeon97 <96443467+sehyeon97@users.noreply.github.com>
2024-11-10 19:51:26 -0800

    Delete lib/api directory for security reasons

--- lib/api/api_keys.dart
+++
@@ -1,6 +0,0 @@
-class APIKeys {
-  static String get googleMaps => 'AIzaSyAHTOQrLKb6SF7ntv-a5xXPnBCb0wI4NQ4';
-  static String get yelp =>
-      'Bearer MVpUg3PhQSaHqt_uWW6arVOePTo-ttyCE6B45PJYNhz5szmmptYzBpEGRUwKH8u5CxZ8piZHGeU-V-gjWwHDlJmeWzT8pHQM8iW3qyRzSd4vP8ZHLyE43oniHSZgZXYx';
-  static String get firebase => 'AIzaSyAEpml3W7iPIfDKhex3h03oYI4nZsAHJX4';
-}
